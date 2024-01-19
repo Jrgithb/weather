27bfd5bfd5ce8feb6a97e199457c3f392494a336
Jrgithub <40182730@hruc.ac.uk>
2024-01-19 16:35:04 +0000

    initial commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,33 @@
+import "./NavBar.css"
+import {useNavigate} from "react-router-dom";
+
+const NavButton = ({ text, path }) => {
+    const navigate = useNavigate()
+
+    return (
+        <button className="nav-button" onClick={() => navigate(path)}>
+            {text}
+        </button>
+    )
+}
+
+const NavBar = () =>{
+    return(
+        <div className="Nav-container">
+        <img src="https://classroomclipart.com/image/static7/preview2/snowflake-intricate-pattern-clip-art-59075.jpg"/>
+             <h1>Health Advice Group</h1>
+
+             <div className="nav-buttons">
+
+                <NavButton text="Find a forecast" path="/Find a forecast" />
+                <NavButton text="Warning and advice" path="/Warning and advice" />
+                <NavButton text="Air quality" path="/Air quality" />
+
+             </div>
+
+
+        </div>
+    );
+};
+
+export default NavBar;
