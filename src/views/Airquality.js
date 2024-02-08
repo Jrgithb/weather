481366a2978ef1182965b98d21349e2ad6f31775
Jrgithb <40182730@hruc.ac.uk>
2024-02-08 12:53:51 +0000

    weather

--- src/views/Airquality.js
+++ src/views/Airquality.js
@@ -3,8 +3,8 @@ import "./Airquality.css"
 const Airquality = () =>{
     return(
         <div className="air-content">
-            <h2 className="air-quality-header">Current air quality</h2>
             <div className="air-quality-content">
+                <h2 className="air-quality-header">Current air quality</h2>
                 <div className="date-content">
                     {/* <p className="day-of-the-week">Today</p>
                     <p className="date">2/2</p> */}
